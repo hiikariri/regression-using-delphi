99913bd6ebe4a40cb1d9f0d29273a0b251c6ac4f
Nehemy Davis <109798265+hiikariri@users.noreply.github.com>
2024-06-24 03:36:49 +0700

    initial commit

---
+++ LinearAndPolynomialRegression5023221004.dpr
@@ -0,0 +1,13 @@
+program LinearAndPolynomialRegression5023221004;
+
+uses
+  Forms,
+  UnitLinearAndPolynomialRegression in 'UnitLinearAndPolynomialRegression.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
